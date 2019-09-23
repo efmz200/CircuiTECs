0072480e7fbed33e0021619796eec15b61a9c839
erick <mazae2000@estudiantec.cr>
2019-09-22 23:19:52 -0600

    Btn Or imagen y funcionalidad

--- Circuit Designer/src/circuit/designer/CircuitDesigner.java
+++ Circuit Designer/src/circuit/designer/CircuitDesigner.java
@@ -18,12 +18,9 @@ import javafx.stage.Stage;
 public class CircuitDesigner extends Application {
     
     @Override
-    public void start(Stage stage) throws Exception {
-        
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
+    public void start(Stage stage) throws Exception {        
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));        
+        Scene scene = new Scene(root);        
         stage.setScene(scene);
         stage.show();
     }
